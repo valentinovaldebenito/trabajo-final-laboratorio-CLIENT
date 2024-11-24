c62bf7697db87b6f3bc7a211acc45c3e3b89d481
valentinovaldebenito <valentinovaldebenito.dev@gmail.com>
2024-11-23 21:48:32 -0300

    Fix: Update y Delete

--- src/views/Products.jsx
+++ src/views/Products.jsx
@@ -17,38 +17,39 @@ function Productos() {
     { label: "Activo", value: 1 },
     { label: "Inactivo", value: 0 },
   ];
-  const [activo, setActivo] = useState(options[0]);
   const [displayCreateDialog, setDisplayCreateDialog] = useState(false);
   const [displayEditDialog, setDisplayEditDialog] = useState(false);
-  //const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [itemEdit, setItemEdit] = useState();
+  const [activo, setActivo] = useState(options[0]);
   const [marca, setMarca] = useState();
   const [descripcion, setDescripcion] = useState();
   const [valorUnidad, setValorUnidad] = useState();
   const [stock, setStock] = useState();
 
-  const products = [
-    { id: 1, marca: "Ilolay", descripcion: "Leche", valorUnidad: 900, stock: 200, activo: 1 },
-    { id: 2, marca: "Ilolay", descripcion: "Leche", valorUnidad: 900, stock: 200, activo: 1 },
-    { id: 3, marca: "Ilolay", descripcion: "Leche", valorUnidad: 900, stock: 200, activo: 1 },
-    { id: 4, marca: "Ilolay", descripcion: "Leche", valorUnidad: 900, stock: 200, activo: 1 },
+  const productsMuestra = [
+    { id: 1, marca: "Ilolay", descripcion: "Leche", valorUnidad: 900, stock: 100, activo: 1 },
+    { id: 2, marca: "La Serenisima", descripcion: "Leche", valorUnidad: 600, stock: 300, activo: 1 },
+    { id: 3, marca: "Sancor", descripcion: "Leche", valorUnidad: 500, stock: 400, activo: 1 },
+    { id: 4, marca: "Las tres hermanas", descripcion: "Leche", valorUnidad: 700, stock: 230, activo: 1 },
   ];
 
   const getProducts = async () => {
     try {
-      const response = await axios.get("http://localhost:3000/products", {});
+      const response = await axios.get("http://localhost:3000/products/getAll", {});
       setProducts(response.data);
     } catch (error) {
       console.error(error);
-      //navigate("/login");
     }
   };
 
+  //Create
   const cargarProducto = async () => {
     if (marca && descripcion && valorUnidad && stock && activo) {
       console.log(marca, descripcion, valorUnidad, stock, activo);
 
       try {
-        const response = await axios.post("http://localhost:3000/products", {
+        const response = await axios.post("http://localhost:3000/product/upload", {
           marca: marca,
           descripcion: descripcion,
           valorUnidad: valorUnidad,
@@ -64,6 +65,7 @@ function Productos() {
     }
   };
 
+  //Delete
   const eliminarProducto = async (id) => {
     console.log(id);
     if (marca && descripcion && valorUnidad && stock && activo) {
@@ -75,31 +77,44 @@ function Productos() {
           stock: stock,
           activo: activo,
         });
-        console.log(response)
+        console.log(response);
       } catch (error) {
         console.error(error);
       }
     }
   };
 
-  const editarProducto = async (id) => {
-    console.log(id);
-    if (marca && descripcion && valorUnidad && stock && activo) {
+  //Update
+  const editarProducto = async () => {
+    if (itemEdit) {
       try {
-        const response = await axios.put(`http://localhost:3000/product/update/:${id}`, {
+        const response = await axios.put(`http://localhost:3000/product/update/:${itemEdit.id}`, {
           marca: marca,
           descripcion: descripcion,
           valorUnidad: valorUnidad,
           stock: stock,
-          activo: activo,
+          activo: 1,
         });
         console.log(response)
-        setDisplayEditDialog(false)
+        setItemEdit({});
+        setDisplayEditDialog(false);
       } catch (error) {
         console.error(error);
       }
+    } else {
+      console.error("Error al obtener el ItemEdit");
     }
-  }
+  };
+
+  const handleEditClick = (rowData) => {
+    setItemEdit(rowData);
+    setMarca(rowData.marca);
+    setDescripcion(rowData.descripcion);
+    setValorUnidad(rowData.valorUnidad);
+    setStock(rowData.stock);
+
+    setDisplayEditDialog(true);
+  };
 
   useEffect(() => {
     getProducts();
@@ -126,7 +141,7 @@ function Productos() {
             </div>
           </div>
           <Card className="flex flex-column w-10 overflow-x-hidden overflow-y-hidden">
-            <DataTable value={products} className="w-full">
+            <DataTable value={productsMuestra} className="w-full">
               <Column field="id" header="Id" />
               <Column field="marca" header="Marca" />
               <Column field="descripcion" header="Descripción" />
@@ -134,10 +149,29 @@ function Productos() {
               <Column field="stock" header="Stock" />
               <Column field="activo" header="Activo" />
               <Column
-                header="Acciones"
                 body={(rowData) => (
-                  <Button icon="pi pi-times" rounded text severity="danger" aria-label="Eliminar" onClick={editarProducto(rowData.id)} />,
-                  <Button icon="pi pi-times" rounded text severity="danger" aria-label="Eliminar" onClick={eliminarProducto(rowData.id)} />
+                  <Button
+                    key={rowData.id}
+                    icon="pi pi-pencil"
+                    rounded
+                    text
+                    severity="primary"
+                    aria-label="Editar"
+                    onClick={() => handleEditClick(rowData)}
+                  />
+                )}
+              />
+              <Column
+                body={(rowData) => (
+                  <Button
+                    key={rowData.id}
+                    icon="pi pi-times"
+                    rounded
+                    text
+                    severity="danger"
+                    aria-label="Eliminar"
+                    onClick={() => eliminarProducto(rowData.id)}
+                  />
                 )}
               />
             </DataTable>
@@ -179,25 +213,38 @@ function Productos() {
       {/* Dialog Para Editar Productos */}
       <Dialog className="w-4" header="Editar Producto" draggable={false} visible={displayEditDialog} onHide={() => setDisplayEditDialog(false)}>
         <div className="flex flex-column align-items-center w-full gap-3">
-          <InputText className="w-full" value={marca} onChange={(e) => setMarca(e.target.value)} placeholder="Marca" />
-          <InputText className="w-full" value={descripcion} onChange={(e) => setDescripcion(e.target.value)} placeholder="Descripción" />
-          <InputNumber
-            className="w-full"
-            value={valorUnidad}
-            onChange={(e) => {
-              setValorUnidad(e.value);
-            }}
-            placeholder="Valor por Unidad"
-          />
-          <InputNumber
-            className="w-full"
-            value={stock}
-            onChange={(e) => {
-              setStock(e.value);
-            }}
-            placeholder="Stock"
-          />
-          <SelectButton className="w-full" value={activo} onChange={(e) => setActivo(e.value)} options={options} />
+          <div className="flex flex-column w-full">
+            <span className="font-semibold mb-2 ml-2">ID Producto</span>
+            <InputText className="w-full" value={"ID Producto: " + itemEdit?.id || null} disabled={true} name="ID" />
+          </div>
+          <div className="flex flex-column w-full">
+            <span className="font-semibold mb-2 ml-2">Marca</span>
+            <InputText className="w-full" value={marca} onChange={(e) => setMarca(e.target.value)} />
+          </div>
+          <div className="flex flex-column w-full">
+            <span className="font-semibold mb-2 ml-2">Descripción</span>
+            <InputText className="w-full" value={descripcion} onChange={(e) => setDescripcion(e.target.value)} />
+          </div>
+          <div className="flex flex-column w-full">
+            <span className="font-semibold mb-2 ml-2">Valor Unitario</span>
+            <InputNumber
+              className="w-full"
+              value={valorUnidad}
+              onChange={(e) => {
+                setValorUnidad(e.value);
+              }}
+            />
+          </div>
+          <div className="flex flex-column w-full">
+            <span className="font-semibold mb-2 ml-2">Cantidad Stock</span>
+            <InputNumber
+              className="w-full"
+              value={stock}
+              onChange={(e) => {
+                setStock(e.value);
+              }}
+            />
+          </div>
 
           <div className="flex w-full justify-end">
             <div className="w-full flex justify-content-end">
